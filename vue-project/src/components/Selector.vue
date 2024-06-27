ea5d3ac9fcb27f4d2544bc9de49997a47e3e3f90
Matvey Baklykov <matvey.bakl@gmail.com>
2024-06-27 15:34:07 +0300

    first commit

---
+++ vue-project/src/components/Selector.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul>
+        <li @click="setCrypto('BTC')" :className="cryptoNow == 'BTC' ? 'active' : ''">Bitcoin</li>
+        <li @click="setCrypto('ETH')" :className="cryptoNow == 'ETH' ? 'active' : ''">ETH</li>
+        <li @click="setCrypto('USDT')" :className="cryptoNow == 'USDT' ? 'active' : ''">USDT</li>
+    </ul>
+</template>
+  
+<script>
+  export default {
+    props: {
+        setCrypto: {
+            type: Function,
+            required: true,
+        },
+        cryptoNow: {
+            type: String,
+            required: true,
+        }
+    },
+    
+  }
+</script>
+  
+<style scoped>
+ul {
+    list-style: none;
+    width: 200px;
+    padding: 0;
+    border-radius: 3px;
+    border: 3px solid #000;
+}
+
+li {
+    display: block;
+    background-color: #1A032D;
+    color: #fff;
+    padding: 20px 0;
+}
+
+li:hover, li.active {
+    background-color: #24043e;
+    cursor: pointer;
+}
+</style>
---
+++ vue-project/src/components/input.vue
@@ -0,0 +1,57 @@
+
+
+<template>
+  <input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Введите число"><br>
+  <button @click="convert()">Конвертировать</button>
+  <button @click="favourite()" className="fav-button">В избранное</button>
+</template>
+
+<script>
+export default {
+    props: {
+        changeAmount: {
+            type: Function,
+            required: true,
+        },
+        convert: {
+            type: Function,
+            required: true,
+        },
+        favourite: {
+            type: Function,
+            required: true,
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+input {
+  border-radius: 3px;
+  border: 0;
+  padding: 10px 15px;
+  background-color: #fafafa;
+  color: #333;
+  outline: none;
+  width: 500px;
+  position: relative;
+  top: -50px;
+  font-size: 1.2em;
+}
+
+button {
+    position: relative;
+    top: -20px;
+    padding: 15px;
+    color: #fff;
+    text-transform: uppercase;
+    cursor: pointer;
+    background-color: #1a032d;
+    border: 0;
+    border-radius: 3px;
+}
+button.fav-button {
+    margin-left: 20px;
+}
+</style>
